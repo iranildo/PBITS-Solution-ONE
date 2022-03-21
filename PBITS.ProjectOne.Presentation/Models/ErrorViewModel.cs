cb5e86d9b6e08f5778ce62768e9ce0d511b90ef3
Iranildo Júnior <iranildo@outlook.com>
2022-03-21 17:31:26 -0300

    PBITS | Criação da Estrutura Inicial

---
+++ PBITS.ProjectOne.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PBITS.ProjectOne.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
