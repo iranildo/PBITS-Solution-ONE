c448f798213986357ef2d5f55e8205c62515d832
Iranildo Júnior <iranildo@outlook.com>
2022-03-23 09:54:48 -0300

    Correções no appsettings para base PBITS e initial migrations

--- PBITS.ProjectOne.Presentation/Data/Migrations/20220322153021_InitialMigrationWithIdentity2.Designer.cs
+++ PBITS.ProjectOne.Presentation/Data/Migrations/20220322153021_InitialMigrationWithIdentity2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PBITS.ProjectOne.Presentation.Data;
 
@@ -11,9 +12,10 @@ using PBITS.ProjectOne.Presentation.Data;
 namespace PBITS.ProjectOne.Presentation.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220322153021_InitialMigrationWithIdentity2")]
+    partial class InitialMigrationWithIdentity2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
